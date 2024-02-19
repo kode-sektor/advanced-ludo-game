1d71a177beed5a377912b199bd3cd8eea94a3e27
kode-sektor <kayodeibiyemi92@gmail.com>
2024-02-18 23:57:33 -0500

    Mark up outer structure of game

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+const App = () => {
+    return (
+        <div className='container-fluid'>
+            <div className='frame'>
+                <section className='base home-one'>
+                    <section className='camp'>
+                        <section className='token-compartment'>
+                        </section>
+                    </section>
+                    <section className='portal'>
+                    </section>
+                    <section className='outpost-lane'>
+                    </section>
+                </section>
+                <section className='base home-one'>
+                    <section className='camp'>
+                        <section className='token-compartment'>
+                        </section>
+                    </section>
+                    <section className='portal'>
+                    </section>
+                    <section className='outpost-lane'>
+                    </section>
+                </section>
+                <section className='base home-one'>
+                    <section className='camp'>
+                        <section className='token-compartment'>
+                        </section>
+                    </section>
+                    <section className='portal'>
+                    </section>
+                    <section className='outpost-lane'>
+                    </section>
+                </section>
+                <section className='base home-one'>
+                    <section className='camp'>
+                        <section className='token-compartment'>
+                        </section>
+                    </section>
+                    <section className='portal'>
+                    </section>
+                    <section className='outpost-lane'>
+                    </section>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default App
