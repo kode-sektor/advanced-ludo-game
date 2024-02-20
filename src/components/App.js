9a54c6c18162399f513ce812f6846a5f38f5a4e5
kode-sektor <kayodeibiyemi92@gmail.com>
2024-02-19 23:56:48 -0500

    Create portal and outpost cells

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,93 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 const App = () => {
     return (
-        <div className='container-fluid'>
-            <div className='frame'>
-                <section className='base home-one'>
-                    <section className='camp'>
-                        <section className='token-compartment'>
+        <div className="container-fluid">
+            <div className="frame">
+            
+                <section className="base home-one">
+                    <section className="outpost-lane">
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                    </section>
+                    <section className="camp">
+                        <section className="token-compartment">
                         </section>
                     </section>
-                    <section className='portal'>
-                    </section>
-                    <section className='outpost-lane'>
+                    <section className="portal">
+                        <section className="lair">
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                        </section>
+                        <section className="portal-cells">
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                            <div className="cell"></div>
+                        </section>    
                     </section>
                 </section>
-                <section className='base home-one'>
-                    <section className='camp'>
-                        <section className='token-compartment'>
+
+                <section className="base home-two">
+                    <section className="outpost-lane">
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                    </section>
+                    <section className="camp">
+                        <section className="token-compartment">
                         </section>
                     </section>
-                    <section className='portal'>
-                    </section>
-                    <section className='outpost-lane'>
+                    <section className="portal">
                     </section>
                 </section>
-                <section className='base home-one'>
-                    <section className='camp'>
-                        <section className='token-compartment'>
+
+                <section className="base home-three">
+                    <section className="outpost-lane">
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                    </section>
+                    <section className="camp">
+                        <section className="token-compartment">
                         </section>
                     </section>
-                    <section className='portal'>
-                    </section>
-                    <section className='outpost-lane'>
+                    <section className="portal">
                     </section>
                 </section>
-                <section className='base home-one'>
-                    <section className='camp'>
-                        <section className='token-compartment'>
+
+                <section className="base home-four">
+                    <section className="outpost-lane">
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                        <div className="cell"></div>
+                    </section>
+                    <section className="camp">
+                        <section className="token-compartment">
                         </section>
                     </section>
-                    <section className='portal'>
-                    </section>
-                    <section className='outpost-lane'>
+                    <section className="portal">
                     </section>
                 </section>
             </div>
