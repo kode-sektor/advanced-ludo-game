b3156619230d1d7d2d0988614821edda6d404afe
kode-sektor <kayodeibiyemi92@gmail.com>
2024-03-11 11:05:56 -0400

    Style dice

--- src/components/App.js
+++ src/components/App.js
@@ -1,234 +1,225 @@
 import React from "react";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
-    return (
-        <div className="board">
-            <div className="ludo">
-                <section className="base home-one">
-                    <section className="outpost-lane">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                    </section>
-                    <section className="camp">
-                        <section className="window">
-                            <div className="cell">
-                                <button className="seed"></button>
-                            </div>
-                            <div className="cell">
-                                <button className="seed"></button>
-                            </div>
-                            <div className="cell">
-                                <button className="seed"></button>
-                            </div>
-                            <div className="cell">
-                                <button className="seed"></button>
-                            </div>
-                        </section>
-                        <section className="imprint">
-                            <span>LUDO</span>
-                        </section>
-                        <section className="imprint">
-                            <span>LUDO</span>
-                        </section>
-                    </section>
-                    <section className="exit">
-                        <section className="lair">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                        </section>
-                        <section className="portal">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                        </section>    
-                    </section>
-                </section>
+	return (
+		<div className="board-game">
+			<section className="board">
+				<div className="ludo">
+					<section className="base home-one">
+						<section className="outpost-lane">
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+						</section>
+						<section className="camp">
+							<section className="window">
+								<div className="cell">
+									<button className="seed"></button>
+								</div>
+								<div className="cell">
+									<button className="seed"></button>
+								</div>
+								<div className="cell">
+									<button className="seed"></button>
+								</div>
+								<div className="cell">
+									<button className="seed"></button>
+								</div>
+							</section>
+							<section className="imprint">
+								<span>LUDO</span>
+							</section>
+							<section className="imprint">
+								<span>LUDO</span>
+							</section>
+						</section>
+						<section className="exit">
+							<section className="lair">
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+							</section>
+							<section className="portal">
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+							</section>
+						</section>
+					</section>
+					<section className="base home-two">
+						<section className="outpost-lane">
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+						</section>
+						<section className="camp">
+							<section className="window">
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+							</section>
+							<section className="imprint">
+								<span>LUDO</span>
+							</section>
+							<section className="imprint">
+								<span>LUDO</span>
+							</section>
+						</section>
+						<section className="exit">
+							<section className="lair">
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+							</section>
+							<section className="portal">
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+							</section>
+						</section>
+					</section>
+					<section className="base home-three">
+						<section className="outpost-lane">
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+						</section>
+						<section className="camp">
+							<section className="window">
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+							</section>
+							<section className="imprint">
+								<span>LUDO</span>
+							</section>
+							<section className="imprint">
+								<span>LUDO</span>
+							</section>
+						</section>
+						<section className="exit">
+							<section className="lair">
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+							</section>
+							<section className="portal">
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+							</section>
+						</section>
+					</section>
+					<section className="base home-four">
+						<section className="outpost-lane">
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+							<div className="cell"></div>
+						</section>
+						<section className="camp">
+							<section className="window">
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+								<div className="cell">
+									<div className="seed"></div>
+								</div>
+							</section>
+							<section className="imprint">
+								<span>LUDO</span>
+							</section>
+							<section className="imprint">
+								<span>LUDO</span>
+							</section>
+						</section>
+						<section className="exit">
+							<section className="lair">
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+							</section>
+							<section className="portal">
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+								<div className="cell"></div>
+							</section>
+						</section>
+					</section>
+					<section className="home"></section>
+				</div>
+			</section>
+		</div>
+	);
+};
 
-                <section className="base home-two">
-                    <section className="outpost-lane">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                    </section>
-                    <section className="camp">
-                        <section className="window">
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                        </section>
-                        <section className="imprint">
-                            <span>LUDO</span>
-                        </section>
-                        <section className="imprint">
-                            <span>LUDO</span>
-                        </section>
-                    </section>
-                    <section className="exit">
-                        <section className="lair">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                        </section>
-                        <section className="portal">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                        </section>    
-                    </section>
-                </section>
-
-                <section className="base home-three">
-                    <section className="outpost-lane">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                    </section>
-                    <section className="camp">
-                        <section className="window">
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                        </section>
-                        <section className="imprint">
-                            <span>LUDO</span>
-                        </section>
-                        <section className="imprint">
-                            <span>LUDO</span>
-                        </section>
-                    </section>
-                    <section className="exit">
-                        <section className="lair">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                        </section>
-                        <section className="portal">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                        </section>    
-                    </section>
-                </section>
-    
-                <section className="base home-four">
-                    <section className="outpost-lane">
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                        <div className="cell"></div>
-                    </section>
-                    <section className="camp">
-                        <section className="window">
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                            <div className="cell">
-                                <div className="seed"></div>
-                            </div>
-                        </section>
-                        <section className="imprint">
-                            <span>LUDO</span>
-                        </section>
-                        <section className="imprint">
-                            <span>LUDO</span>
-                        </section>
-                    </section>
-                    <section className="exit">
-                        <section className="lair">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                        </section>
-                        <section className="portal">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                            <div className="cell"></div>
-                        </section>    
-                    </section>
-                </section>
-                <section className="home"></section>
-            </div>
-            <div className="dice">
-                <div className="side"></div>
-                <div className="side"></div>
-                <div className="side"></div>
-                <div className="side"></div>
-                <div className="side"></div>
-                <div className="side"></div>
-            </div>
-        </div>
-    )
-}
-
-export default App
+export default App;
