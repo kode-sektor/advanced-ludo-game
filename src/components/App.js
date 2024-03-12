a4bc2be0b04f57de8af4738e25e07c0f07f442ad
kode-sektor <kayodeibiyemi92@gmail.com>
2024-03-12 10:30:45 -0400

    Title cell paths

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,12 @@ const App = () => {
 				<div className="ludo">
 					<section className="base home-one">
 						<section className="outpost-lane">
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
+							<div className="cell" title="11"></div>
+							<div className="cell" title="10"></div>
+							<div className="cell" title="9"></div>
+							<div className="cell" title="8"></div>
+							<div className="cell" title="7"></div>
+							<div className="cell" title="6"></div>
 						</section>
 						<section className="camp">
 							<section className="window">
@@ -40,13 +40,13 @@ const App = () => {
 						</section>
 						<section className="exit">
 							<section className="lair">
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
+								<div className="cell" title="6"></div>
+								<div className="cell" title="5"></div>
+								<div className="cell" title="4"></div>
+								<div className="cell" title="3"></div>
+								<div className="cell" title="2"></div>
+								<div className="cell" title="1"></div>
+								<div className="cell" title="52"></div>
 							</section>
 							<section className="portal">
 								<div className="cell"></div>
@@ -55,18 +55,18 @@ const App = () => {
 								<div className="cell"></div>
 								<div className="cell"></div>
 								<div className="cell"></div>
-								<div className="cell"></div>
+								<div className="cell" title="51"></div>
 							</section>
 						</section>
 					</section>
 					<section className="base home-two">
 						<section className="outpost-lane">
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
+							<div className="cell" title="24"></div>
+							<div className="cell" title="23"></div>
+							<div className="cell" title="22"></div>
+							<div className="cell" title="21"></div>
+							<div className="cell" title="20"></div>
+							<div className="cell" title="19"></div>
 						</section>
 						<section className="camp">
 							<section className="window">
@@ -93,32 +93,32 @@ const App = () => {
 						<section className="exit">
 							<section className="lair">
 								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
+								<div className="cell" title="18"></div>
+								<div className="cell" title="17"></div>
+								<div className="cell" title="16"></div>
+								<div className="cell" title="15"></div>
+								<div className="cell" title="14"></div>
+								<div className="cell" title="13"></div>
 							</section>
 							<section className="portal">
+								<div className="cell" title=""></div>
 								<div className="cell"></div>
 								<div className="cell"></div>
 								<div className="cell"></div>
 								<div className="cell"></div>
 								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
+								<div className="cell" title="12"></div>
 							</section>
 						</section>
 					</section>
 					<section className="base home-three">
 						<section className="outpost-lane">
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
+							<div className="cell" title="37"></div>
+							<div className="cell" title="36"></div>
+							<div className="cell" title="35"></div>
+							<div className="cell" title="34"></div>
+							<div className="cell" title="33"></div>
+							<div className="cell" title="32"></div>
 						</section>
 						<section className="camp">
 							<section className="window">
@@ -145,32 +145,32 @@ const App = () => {
 						<section className="exit">
 							<section className="lair">
 								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
+								<div className="cell" title="31"></div>
+								<div className="cell" title="30"></div>
+								<div className="cell" title="29"></div>
+								<div className="cell" title="28"></div>
+								<div className="cell" title="27"></div>
+								<div className="cell" title="26"></div>
 							</section>
 							<section className="portal">
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
+								<div className="cell" title=""></div>
+								<div className="cell" title=""></div>
+								<div className="cell" title=""></div>
+								<div className="cell" title=""></div>
+								<div className="cell" title=""></div>
+								<div className="cell" title=""></div>
+								<div className="cell" title="25"></div>
 							</section>
 						</section>
 					</section>
 					<section className="base home-four">
 						<section className="outpost-lane">
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
-							<div className="cell"></div>
+							<div className="cell" title="50"></div>
+							<div className="cell" title="49"></div>
+							<div className="cell" title="48"></div>
+							<div className="cell" title="47"></div>
+							<div className="cell" title="46"></div>
+							<div className="cell" title="45"></div>
 						</section>
 						<section className="camp">
 							<section className="window">
@@ -196,13 +196,13 @@ const App = () => {
 						</section>
 						<section className="exit">
 							<section className="lair">
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
-								<div className="cell"></div>
+								<div className="cell" title=""></div>
+								<div className="cell" title="44"></div>
+								<div className="cell" title="43"></div>
+								<div className="cell" title="42"></div>
+								<div className="cell" title="41"></div>
+								<div className="cell" title="40"></div>
+								<div className="cell" title="39"></div>
 							</section>
 							<section className="portal">
 								<div className="cell"></div>
@@ -211,7 +211,7 @@ const App = () => {
 								<div className="cell"></div>
 								<div className="cell"></div>
 								<div className="cell"></div>
-								<div className="cell"></div>
+								<div className="cell" title="38"></div>
 							</section>
 						</section>
 					</section>
