e317dc4320c41ed349e7063016b3a45aad084a87
kode-sektor <kayodeibiyemi92@gmail.com>
2024-03-28 20:31:40 -0400

    Combine breakout and regular move (1)

--- src/components/App.js
+++ src/components/App.js
@@ -16,56 +16,78 @@ export default class App extends Component {
 	turningPoints = [4, 10, 12, 17, 23, 25, 30, 36, 38, 43, 49];
 	diagonals = [4, 17, 30, 43];
 
-	direction = (cell, dieVal, id) => {
+	northward = [
+		...[...Array(4)].map((_, i) => 0 + i),
+		...[...Array(12 - 10)].map((_, i) => 10 + i),
+		...[...Array(23 - 18)].map((_, i) => 18 + i)
+	];	// (11) [0, 1, 2, 3, 10, 11, 18, 19, 20, 21, 22]
+	southward = [
+		...[...Array(30 - 25)].map((_, i) => 25 + i),
+		...[...Array(38 - 36)].map((_, i) => 36 + i),
+		...[...Array(50 - 45)].map((_, i) => 45 + i)
+	];	// (12) [25, 26, 27, 28, 29, 36, 37, 45, 46,...]
+	eastward = [
+		...[...Array(17 - 12)].map((_, i) => 12 + i),
+		...[...Array(25 - 23)].map((_, i) => 23 + i),
+		...[...Array(36 - 30)].map((_, i) => 30 + i)
+	];
+	westward = [
+		...[...Array(10 - 5)].map((_, i) => 5 + i),
+		...[...Array(25 - 23)].map((_, i) => 23 + i),
+		...[...Array(36 - 30)].map((_, i) => 30 + i)
+	];
+
+	calcDirection = (id, cell, dieVal, cellPaths) => {
 		let startCell = cell;
 		let finalCell = cell + dieVal;
+		let filteredCellRange = [];
 
-		let x = "";
-		let y = "";
+		// let x = "";
+		// let y = "";
 
-		filterCellRange = () => {
-			let config = { startCell: 0, finalCell: 15 };
-
-			const filteredCellRange = this.turningPoints.filter((item) => {
-				return item > config.startCell && item < config.finalCell;
+		const filterCellRange = () => {
+			filteredCellRange = this.turningPoints.filter((item) => {
+				return item > startCell && item < finalCell;
 			});
-			// console.log(filteredCellRange);	// [4, 10, 12]
+			console.log(filteredCellRange);	// [4, 10, 12]
 		}
 
-		const cellPaths = [];
-		for (let i = 0; i < filteredCellRange.length; i++) {
+		filterCellRange();
+
+		for (let i = 0; i <= filteredCellRange.length; i++) {
 			if (i === 0) {
 				cellPaths.push(filteredCellRange[i]);	// 
-			} else if (i === filteredCellRange.length - 1) {	// 4
-				cellPaths.push(finalCell - filteredCellRange[i]);	// 10 - 4, 12 - 10
+			} else if (i === filteredCellRange.length) {	// 4
+				cellPaths.push(finalCell - filteredCellRange[i - 1]);	// 15 - 12
 			} else {
-				filteredCellRange[i] - filteredCellRange[i - 1];	// 15 - 12
+				cellPaths.push(filteredCellRange[i] - filteredCellRange[i - 1]);	// 10 - 4, 12 - 10
 			}
 		}
 		console.log(cellPaths); 	// [4, 6, 2, 3]
+		return cellPaths;
 
-		if (this.state.players[`${id}`].cell <= 4 ||
-			((this.state.players[`${id}`].cell >= 17) && this.state.players[`${id}`].cell <= 23) ||
-			(this.state.players[`${id}`].cell >= 10 && this.state.players[`${id}`].cell <= 12)) {
-			x = this.state.players[`${id}`].coordinates.x;
-			y = -y;	// die value move
-		}
-		else if ((this.state.players[`${id}`].cell > 5 && this.state.players[`${id}`].cell <= 10) ||
-			this.state.players[`${id}`].cell > 38 && this.state.players[`${id}`].cell <= 43) {
-			x = -x;
-			y = this.state.players[`${id}`].coordinates.y;
+		// if (this.state.players[`${id}`].cell <= 4 ||
+		// 	((this.state.players[`${id}`].cell >= 17) && this.state.players[`${id}`].cell <= 23) ||
+		// 	(this.state.players[`${id}`].cell >= 10 && this.state.players[`${id}`].cell <= 12)) {
+		// 	x = this.state.players[`${id}`].coordinates.x;
+		// 	y = -y;	// die value move
+		// }
+		// else if ((this.state.players[`${id}`].cell > 5 && this.state.players[`${id}`].cell <= 10) ||
+		// 	this.state.players[`${id}`].cell > 38 && this.state.players[`${id}`].cell <= 43) {
+		// 	x = -x;
+		// 	y = this.state.players[`${id}`].coordinates.y;
 
-		} else if ((this.state.players[`${id}`].cell >= 12 && this.state.players[`${id}`].cell <= 17) ||
-			(this.state.players[`${id}`].cell > 23 && this.state.players[`${id}`].cell <= 25) ||
-			(this.state.players[`${id}`].cell > 30 && this.state.players[`${id}`].cell <= 36)) {
-			x = x;
-			y = this.state.players[`${id}`].coordinates.y;
-		} else if ((this.state.players[`${id}`].cell > 25 && this.state.players[`${id}`].cell <= 30) ||
-			(this.state.players[`${id}`].cell > 36 && this.state.players[`${id}`].cell <= 38) ||
-			(this.state.players[`${id}`].cell > 43 && this.state.players[`${id}`].cell <= 49)) {
-			x = this.state.players[`${id}`].coordinates.x;
-			y = y;
-		}
+		// } else if ((this.state.players[`${id}`].cell >= 12 && this.state.players[`${id}`].cell <= 17) ||
+		// 	(this.state.players[`${id}`].cell > 23 && this.state.players[`${id}`].cell <= 25) ||
+		// 	(this.state.players[`${id}`].cell > 30 && this.state.players[`${id}`].cell <= 36)) {
+		// 	x = x;
+		// 	y = this.state.players[`${id}`].coordinates.y;
+		// } else if ((this.state.players[`${id}`].cell > 25 && this.state.players[`${id}`].cell <= 30) ||
+		// 	(this.state.players[`${id}`].cell > 36 && this.state.players[`${id}`].cell <= 38) ||
+		// 	(this.state.players[`${id}`].cell > 43 && this.state.players[`${id}`].cell <= 49)) {
+		// 	x = this.state.players[`${id}`].coordinates.x;
+		// 	y = y;
+		// }
 	}
 
 	updatePosition = (id, diceVal) => {
@@ -91,37 +113,83 @@ export default class App extends Component {
 		return diceValues;
 	}
 
-	move({ x, y }, e) {
+	move(e) {
+		console.log(e)
 		const id = (e.currentTarget.id);
+		const cellPaths = [];
+		this.calcDirection(id, 0, 15, cellPaths);
 
-		let dieVal = this.randomDice();	// get dice Value
-		dieVal = dieVal[0];	// for testing purpose, get 1 die Value
-		console.log(dieVal);
-		this.updatePosition(id, dieVal);	// Update cell position
+		// let dieVal = this.randomDice(15, id);	// get dice Value
+		// dieVal = dieVal[0];	// for testing purpose, get 1 die Value
+		// console.log(dieVal);
+		// this.updatePosition(id, dieVal);	// Update cell position
 
-		const initMove = (counter, displacement, dir) => {
+		// const initMove = (counter, displacement, dir) => {
+		// 	// Determine direction of travel. For diagonal travel, x & y must update in state at the same time
+		// 	dir = (displacement==="object") ? "diagonal" : dir;	
+		// 	console.log(dir);
+
+		// 	if (counter < 2) {
+		// 		setTimeout(() => {
+		// 			this.setState({
+		// 				...this.state,
+		// 				coordinates: {
+		// 					...this.state.coordinates,
+		// 					x: dir === "diagonal" ? this.state.coordinates.x + displacement.x : dir === "horiz" ? this.state.coordinates.x + displacement : this.state.coordinates.x,
+		// 					y: dir === "diagonal" ? this.state.coordinates.y + displacement.y : dir === "vert" ? this.state.coordinates.y + displacement : this.state.coordinates.y
+		// 				},
+		// 				activeId: counter === 0 ? id : this.state.activeId,	// select seed to move
+		// 				inMotion: counter === 1 ? false : true    // higher z-index when seed is in motion
+		// 			});
+		// 			counter++;
+		// 			initMove(counter, x, "horiz");
+		// 		}, counter === 0 ? 10 : 500);	// avoid setTimeout's first delay, then match transition's duration in css
+		// 	}
+		// }
+		// initMove(0 , y, "vert");
+		
+		const initMove = (counter, displacement, dir="") => {
 			// Determine direction of travel. For diagonal travel, x & y must update in state at the same time
-			dir = (displacement==="object") ? "diagonal" : dir;	
-			console.log(dir);
+			let x = "";
+			let y = "";
+			console.log('displacement', displacement);
 
-			if (counter < 2) {
-				setTimeout(() => {
-					this.setState({
-						...this.state,
-						coordinates: {
-							...this.state.coordinates,
-							x: dir === "diagonal" ? this.state.coordinates.x + displacement.x : dir === "horiz" ? this.state.coordinates.x + displacement : this.state.coordinates.x,
-							y: dir === "diagonal" ? this.state.coordinates.y + displacement.y : dir === "vert" ? this.state.coordinates.y + displacement : this.state.coordinates.y
-						},
-						activeId: counter === 0 ? id : this.state.activeId,	// select seed to move
-						inMotion: counter === 1 ? false : true    // higher z-index when seed is in motion
-					});
-					counter++;
-					initMove(counter, x, "horiz");
-				}, counter === 0 ? 10 : 500);	// avoid setTimeout's first delay, then match transition's duration in css
+			displacement *= displacement;
+
+			this.updatePosition(id, cellPath);	// Update cell position
+			if (this.northward.includes(cellPath)) {
+				x = this.state.coordinates.x;
+				y = -displacement;
+			} else if (this.southward.includes(cellPath)) {
+				x = this.state.coordinates.x;
+				y = displacement;
+			} else if (this.eastward.includes(cellPath)) {
+				x = displacement;
+				y = this.state.coordinates.y;
+			} else if (this.westward.includes(cellPath)) {
+				x = -displacement;
+				y = this.state.coordinates.y;
 			}
+
+			setTimeout(() => {
+				this.setState({
+					...this.state,
+					coordinates: {
+						...this.state.coordinates,
+						x: x,
+						y: y
+					},
+					activeId: counter === 0 ? id : this.state.activeId,	// select seed to move
+					inMotion: counter === 1 ? false : true    // higher z-index when seed is in motion
+				});
+				counter++;
+			}, counter === 10000);	// avoid setTimeout's first delay, then match transition's duration in css
 		}
-		initMove(0 , y, "vert");
+		let cellPath = 0;
+		while (cellPath < cellPaths.length) {
+			initMove(0, cellPaths[cellPath]);
+			cellPath++;
+		};
 	}
 
 	render() {
@@ -144,7 +212,7 @@ export default class App extends Component {
 										<button
 											style={{ transform: this.state.activeId === "seedOne" && `translate(${this.state.coordinates.x}vh, ${this.state.coordinates.y}vh)` }}
 											onClick={(e) => { 
-												this.move({x: 19.8, y: 19.8}, e) }
+												this.move(e) }
 											}
 											className={this.state.inMotion ? "moving seed" : "seed"} id="seedOne">
 										</button>
