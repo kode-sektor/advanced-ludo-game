eaed4dd2f0c1176b818feef4b470e9d54452836d
kode-sektor <kayodeibiyemi92@gmail.com>
2024-05-21 13:26:39 -0400

    Convert RollBtn and related functions into a component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import { players } from "../data/players.js";
 import { TURNING_POINTS, DIAGONALS, CELL_SPEED, CARDINAL_POINTS } from "../data/constants.js"
 
+import RollBtn from '../components/RollBtn/RollBtn.js'
+
 export default class App extends Component {
 
 	constructor() {
@@ -27,6 +29,16 @@ export default class App extends Component {
 				rollDuration: 0
 			}
 		}
+	}
+
+	setDice = (diceObj) => {
+		this.setState({
+			...this.state,
+			dice: {
+				...this.state.dice,
+				...diceObj
+			}
+		})
 	}
 
 	fragmentMove = (id, startCell, finalCell, cellPaths) => {
@@ -79,20 +91,6 @@ export default class App extends Component {
 			inMotion: true,    // higher z-index when seed is in motion
 			transitionDuration : duration 
 		})
-	}
-
-	randomDice = (diceValues=[]) => {
-		for (let dicethrow = 0; dicethrow < 2; dicethrow++) {
-			diceValues.push(this.getRandomWithinRange(1, 6, true));	// Between 1 and 6
-			
-			// Check for double-six (You will do this for the Ludo-bot)
-			/* if (dicethrow === 1) {
-				if (diceValues.slice(-2)[0] === 6 && diceValues.slice(-2)[1] === 6) {
-					this.randomDice(diceValues)
-				}
-			} */
-		}
-		return diceValues;
 	}
 
 	move = (e) => {
@@ -225,442 +223,8 @@ export default class App extends Component {
 		}
 		initMove(this.state.players[`${id}`].cell);
 	}
-
-	getRandomWithinRange = (min, max, int=false) => {
-		let result = 0;
-		if (int) {
-			min = Math.ceil(min);
-			max = Math.floor(max);
-		}
-		result = int ?	// Random integer otherwise float
-			Math.floor(Math.random() * (max - min + 1)) + min :
-			Math.round(((Math.random() * (max - min)) + min) * 100) / 100;
-		
-		return result;	//
-	}
-
-	getDiceCycle = () => {
-		const cycleSteps = [];
-		cycleSteps[0] = this.getRandomWithinRange(1, 3, true);	// 3
-		cycleSteps[1] = this.getRandomWithinRange(1, 3, true);	// 3
-
-		return cycleSteps;	// [1, 3]
-	}
-
-	getDiceTimeout = (cycleSteps) => {
-		let timeoutSegment = 0;
-		let randomTimeout = this.getRandomWithinRange(1.5, 3);
-		// console.log("randomTimeout : ", randomTimeout);
-		
-		const diceTimeout = ([...Array(cycleSteps)].map((_, i) => 0 + i)).map((value, index) => {
-				let pcntStepRollDuration = this.getRandomWithinRange(16.7 / 100, 80 / 100);	// Between 16.7% and 80%
-				// console.log("pcntStepRollDuration : ", pcntStepRollDuration);
-				// console.log("randomTimeout : ", randomTimeout);
-				// console.log("timeoutSegment : ", timeoutSegment);
-				// console.log("randomTimeout - timeoutSegment", (randomTimeout - timeoutSegment))
-
-				/*
-					WHAT WE WANT TO ACHIEVE
-					- Imagine a 3-sequence roll of a die having a 3-second duration
-					- Assume a flat random figure of 0.5, 0.5 and 0.5
-						0.5 of (3 - 0) = 1.5s for 1st sequence
-						0.5 of (3 - 1.5) = 0.75s for 2nd sequence
-						0.5 of (3 - 1.5 - 0.75) = 0.375s for 3rd sequence
-						
-					- But remember total sequence must equal 3 seconds, and 1.5s + 0.75s + 0.375s = 2.625s
-					- Hence the last sequence must be the total minus the 1st and 2nd sequence (3s - (1.5s + 0.75s)) = 0.75s
-				*/
-				/*
-					The problem with this is that it increments the 
-					timeoutSegment += pcntStepRollDuration * (randomTimeout - timeoutSegment);
-					return timeoutSegment;
-					Do the following instead:
-
-					Take care of last loop to ensure 3rd sequence matches total specified duration (e.g. 3 seconds)
-					By making pcntStepRollDuration = 1, this ensures the remaining sequence helps complete the 3-s duration
-					because 1 is a whole number, not a fraction like the others
-				*/
-				pcntStepRollDuration = index === Array(cycleSteps).length - 1 ? 1 : pcntStepRollDuration;
-
-				value = pcntStepRollDuration * (randomTimeout - timeoutSegment);
-				timeoutSegment += value;
-				return Math.round(value * 100) / 100;
-			}
-		)
-		return diceTimeout;
-	}
-
-	/*
-		diceTimeout = [
-			[1.08, 1.63]
-	    	[1.3, 0.91]
-		]
-	*/
-	computeDiceData = (diceTimeout) => {
-
-		let diceTimeoutSlice = diceTimeout.slice();
-		console.log("diceTimeout: ", diceTimeoutSlice);
-		const timeout = [];
-		const duration = [];
-		const dice = [];
-
-		let lastMaxCycle = false;
-
-		/* 
-			Control for switching the dice and transition. This is done because the code in computeMinDiceData()
-			picks the greater element as the max array. Hence the first timeout on die 1 will always be longer
-			(spin longer) than die 2's. To distort this predictability, die 1 and die 2 will be switched
-
-			To solve this, instead of generating a random number afresh, a part of the code is leveraged upon 
-		*/
-		let dieFlip = (diceTimeout[0][0]).toFixed(1).slice(-1) < 5 ? false : true;	
-
-		let dieRollOneSum = 0;
-		let dieRollTwoSum = 0;
-		let diceTimeoutSum = 0;
-		let maxDiceTimeoutSum = 0;
-
-		let minDiceCycle = [];
-		let maxDiceCycle = [];
-
-		// Ensure maxDiceCycle is the array with the larger first element
-		if (diceTimeout[0][0] > diceTimeout[1][0]) {
-			minDiceCycle = diceTimeout[1];
-			maxDiceCycle = diceTimeout[0];
-		} else {
-			minDiceCycle = diceTimeout[0];
-			maxDiceCycle = diceTimeout[1];
-		}
-
-		// Ensure maxDiceCycle's length is never shorter than minDiceCycle's length
-		// This guarantees minDiceCycle's loops will be completed
-		const diceCycleDifference = minDiceCycle.length - maxDiceCycle.length;
-		if (diceCycleDifference > 0) {
-			maxDiceCycle = maxDiceCycle.concat([0]);
-		}
-		dieRollOneSum += maxDiceCycle[0];
-		dieRollTwoSum += minDiceCycle[0];
-
-		// Since the idea is both dice are thrown at the same time, thus, on the first loop, both dice timeouts
-		// must be calculated and set in state (later on)
-
-		// Add the smaller of first values of the 2 arrays
-		timeout.push(Math.min(dieRollOneSum, dieRollTwoSum));
-		duration.push(
-			[
-				maxDiceCycle[0],
-				minDiceCycle[0]
-			]
-		);
-		dice.push([1, 2]);
-		diceTimeoutSum += Math.min(dieRollOneSum, dieRollTwoSum);
-		minDiceCycle.shift();
-		maxDiceCycle.shift();
-
-		// Main code that packages the timeout, duration and corresponding dice
-		// into arrays.
-		const compileTimeout = (diff, dur, die) => {
-			if (dieFlip) {	// Ensure die 1 and die 2 spin lengths are random
-				if (die === 1) {
-					die = 2;
-				} else {
-					die = 1;
-				}
-			}
-			if (diff < 0.1) {
-				timeout[timeout.length - 1] = timeout[timeout.length - 1] + diff;
-				duration[duration.length - 1] = [duration[duration.length - 1], dur];
-				dice[dice.length - 1] = [dice[dice.length - 1], die];
-			} else {
-				timeout.push(diff);
-				duration.push(dur);
-				dice.push(die);
-			}
-			diceTimeoutSum += diff;
-		}
-
-		const computeMinDiceData = () => {
-
-			// Loop across the shorter of the 2 arrays
-			for (let minDieCycle = 0; minDieCycle < minDiceCycle.length; minDieCycle++) {
-				const currMinDieCycle = minDiceCycle[minDieCycle];
-				// Calculate total duration of min array
-				dieRollTwoSum += currMinDieCycle;
-
-				if (dieRollOneSum > dieRollTwoSum) {
-					if (lastMaxCycle && (minDieCycle === minDiceCycle.length - 1)) {
-						maxDiceTimeoutSum = Math.max(dieRollOneSum, dieRollTwoSum);
-						compileTimeout(maxDiceTimeoutSum - diceTimeoutSum, currMinDieCycle, 2);
-					}
-					else {
-						compileTimeout(dieRollTwoSum - diceTimeoutSum, currMinDieCycle, 2);						
-					}	
-				} else {
-					// If on last cycle on minDiceCycle and minDiceCycle is lengthier than maxDiceCycle,
-					// use larger of two timeouts to calculate last timeout
-					if (lastMaxCycle && (minDieCycle === minDiceCycle.length - 1)) {
-						maxDiceTimeoutSum = Math.max(dieRollOneSum, dieRollTwoSum);
-						compileTimeout(maxDiceTimeoutSum - diceTimeoutSum, currMinDieCycle, 2);
-					} else if (lastMaxCycle) {
-						compileTimeout(dieRollTwoSum - diceTimeoutSum, currMinDieCycle, 2);
-					} else {
-						compileTimeout(dieRollOneSum - diceTimeoutSum,  currMinDieCycle, 2);
-					}
-					/*
-						Cut off looped items off minDice arrray which automatically
-						exits loop and onto the maxDiceCycle (so long maxDiceCycle exists!)
-
-						The current iteration of the parent array if 0 means the parent array (maxDiceArray) is 
-						shorter than the child array (minDiceArray). Hence, if parent array is shorter,
-						no dropping off this inner loop until the loops running on the inner is completed
-					*/
-					!lastMaxCycle && minDiceCycle.splice(0, minDieCycle + 1);	
-				}
-			}
-		}
-		// Loop across the lengthier of the 2 arrays
-		for (let maxDieCycle = 0; maxDieCycle < maxDiceCycle.length; maxDieCycle++) {
-			const currMaxDieCycle = maxDiceCycle[maxDieCycle];
-
-			// Toggle 'lastMaxCycle' (watches for last cycle on parent loop if shorter than min array)
-			// and switch true on first loop
-			(currMaxDieCycle === 0) && (lastMaxCycle = true);
-			/*
-				This condition (before running loop on minDiceArray) exists here arising due to predictable steps
-				during the loop process:
-				First, the maxDiceArray is deliberately made the parent array and it has the larger first element
-				of the 2 arrays. Hence (dieRollOneSum > dieRollTwoSum) will always hold true on the very first 
-				loop.
-				Second, because minDiceCycle's first element (dieRollSum) is the lesser value, this inner loop gets processed
-				and based on the condition it's dieRollSums becomes greater, it then drops back to the parent array.
-				Third, after processing in the parent array, the condition to throw the process back into the inner array
-				is the condition being dieRollOneSum is larger than dieRollTwoSum. If it's not, the loop keeps running only
-				on the parent array, eschewing the inner array
-			*/
-			if (dieRollOneSum > dieRollTwoSum) {
-				computeMinDiceData();
-			}	
-
-			// Skip if last element in parent array reaches. Remember 
-			if (!lastMaxCycle) {
-				/*
-					An appended '0' to the maximum dice (parent) array is the condition where the minDiceArray has more
-					elements. But why? Because there's a shift() method run on the array before the loop, thus if the 
-					parent array had only one child, the whole loop process would not even run:
-				*/
-				// Condition to ensure ensuing code does not run when maxDiceArray elements are used up
-				if (currMaxDieCycle !== 0) {
-					/*Only loop across max dice array in the event min dice array completes cycles
-					If max dice array completes cycles first, the min array (above) would run
-					completely and not break out because it is a nested loop.*/
-					dieRollOneSum += currMaxDieCycle;
-				}
-				if (minDiceCycle.length) {	// Child array still exists
-					if (dieRollOneSum > dieRollTwoSum) {
-						compileTimeout(dieRollTwoSum - diceTimeoutSum, currMaxDieCycle, 1);
-					} else {
-						compileTimeout(dieRollOneSum - diceTimeoutSum,  currMaxDieCycle, 1);
-					}
-				} else {	// Child array no longer exists
-					// Last cycle on both arrays
-					if (maxDieCycle === maxDiceCycle.length - 1) {
-						if (dieRollOneSum > dieRollTwoSum) {	// Use larger of die roll sums
-							compileTimeout(dieRollOneSum - diceTimeoutSum,  currMaxDieCycle, 1);
-						} else {
-							compileTimeout(dieRollTwoSum - diceTimeoutSum,  currMaxDieCycle, 1);
-						}
-					} else {	// Child (minDiceArray completed) array but parent array loop continues
-						compileTimeout(dieRollOneSum - diceTimeoutSum,  currMaxDieCycle, 1);
-					}
-				}
-				/*
-					If maxDiceArray needs to loop twice in a series when minDiceArray is yet to complete loops,
-					sidestep minDiceArray to run loop only on parent array and when done with loop running only
-					on maxDiceArray, switch back into child loop.
-
-					This has been explained extensively in the comments some lines above (at the start of the child loop)
-					But this presents a structural problem because on the final loop on the parent array, the process
-					cannot climb back into the child array.
-					
-					Unless of course, the inner loop was turned into a function and called here (last loop on maxDiceArray)
-					just like so: (Must be placed at very bottom of parent loop)
-				*/
-				if (minDiceCycle.length && (maxDieCycle === maxDiceCycle.length - 1)) {
-					computeMinDiceData();
-					lastMaxCycle = true;	// Explicitly set flag because tracking last max diecycle by loop is tricky
-				}
-			}
-		}
-		return [timeout, duration, dice];
-	}
-
 	
-	roll = () => {
 
-		const mapDice = (value) => {
-			
-			let dieTransformX = 0;
-			let dieTransformY = 0;
-			let dieTransformZ = 0;
-
-			const randomTransforms = [360, 0, -360];
-			const diceTransformMap = {	// [x, y coordinates of die value]
-				1 : [0, 0],
-				2 : [270, 0], 
-				3 : [0, 90],
-				4 : [0, 270],
-				5 : [90, 0],
-				6 : [0, 180]    // Also [180, 0]
-			}
-
-			dieTransformX = diceTransformMap[value][0] + randomTransforms[this.getRandomWithinRange(0, 2, true)] + this.getRandomWithinRange(0, 10);
-			/*
-				If die value is 2 or 5, run random transform-Y value because die will roll on Y-axis,
-				hence the current die face stays the same.
-				Then flip transform-Z + or -360deg to make a complete spin and return to current die face
-				
-				This piece of code (this.getRandomWithinRange(0, 10)) gets a random number between 0 and 10 to just tilt
-				the die slightly in random direction to add visual depth. The maximum possible tilt though is not 10 deg
-				but 20 deg because 2 transforms are at play on a die to make the tilt. For e.g, if the die value is 3, 
-				dieTransformX and dieTransformZ if in the same direction and with the maximum possible tilt (10deg), would
-				add to each other to become 20deg
-			*/
-			if (value === 2 || value === 5) {
-				dieTransformY = this.getRandomWithinRange(-400, 400);
-				dieTransformZ = randomTransforms[this.getRandomWithinRange(0, 2, true)] + this.getRandomWithinRange(0, 10);
-			} else if (value === 3 || value === 4) {
-				dieTransformX = randomTransforms[this.getRandomWithinRange(0, 2, true)] + this.getRandomWithinRange(0, 10);
-				dieTransformY = diceTransformMap[value][1] + randomTransforms[this.getRandomWithinRange(0, 2, true)] + this.getRandomWithinRange(1, 10);
-				dieTransformZ = randomTransforms[this.getRandomWithinRange(0, 2, true)] + this.getRandomWithinRange(0, 10);
-			}
-			else {
-				dieTransformY = diceTransformMap[value][1] + randomTransforms[this.getRandomWithinRange(0, 2, true)] + this.getRandomWithinRange(0, 10);
-				dieTransformZ = this.getRandomWithinRange(-400, 400);
-			}
-			return [dieTransformX, dieTransformY, dieTransformZ];
-		}
-
-		const diceValues = [];
-		this.randomDice(diceValues);
-		const mappedDieTransforms = [mapDice(diceValues[0]), mapDice(diceValues[1])];		
-
-		let cycleSteps = [];	
-		let diceTimeout = [];
-		
-		cycleSteps = this.getDiceCycle();	// [3, 3]
-
-		diceTimeout[0] = this.getDiceTimeout(cycleSteps[0]);
-		diceTimeout[1] = this.getDiceTimeout(cycleSteps[1]);
-
-		// diceTimeout[0] = [1.42, 0.45, 0.35];
-		// diceTimeout[1] = [0.7, 1.09, 1.9];
-
-		const diceData = this.computeDiceData(diceTimeout);
-		/*
-			Returns something like: 
-			[
-				[0.7, 0.72, 0.44999999999999996, 1.82] 	 // timeout (settransition loop time)
-				[Array(2), 1.09, Array(2), 0.35]	// die transition duration (css)	
-				[Array(2), 2, Array(2), 1]	// dice 
-			]
-		*/
-		console.log(diceData);
-
-		const cycleStep = diceData[0].length;
-
-		let diceArrLastCycle = 0;
-		let dieOneLastCycle = 0;
-		let dieTwoLastCycle = 0;
-		
-		// Catch loop that maps as last loop to original diceTimeout array
-		/* 
-			Determine index of last entries of die 1 and 2.
-			These last indexes of the die 1 and 2 enables the correct application of die value position
-			on final timeout
-		*/
-		if (Array.isArray(diceData[2][diceData[2].length - 1])) {	
-			dieOneLastCycle = diceData[2][diceData[2].length - 1];
-			dieTwoLastCycle = diceData[2][diceData[2].length - 1];
-		} else {
-			diceArrLastCycle = diceData[2].findLastIndex((element) => Array.isArray(element) === true);
-			dieOneLastCycle = diceData[2].findLastIndex((element) => element === 1);
-			dieTwoLastCycle = diceData[2].findLastIndex((element) => element === 2);
-
-			dieOneLastCycle = (dieOneLastCycle > diceArrLastCycle) ? dieOneLastCycle : diceArrLastCycle;
-			dieTwoLastCycle = (dieTwoLastCycle > diceArrLastCycle) ? dieTwoLastCycle : diceArrLastCycle;
-		}
-
-		const randomRoll = (step) => {
-
-			if (step < cycleStep) {
-				
-				setTimeout(() => {
-					let currDuration = diceData[1][step];
-					let currDice = diceData[2][step];
-					let diceObj = {};
-
-					let transformVals = []
-					for (let i = 0; i < (Array.isArray(currDice) ? 6 : 3); i++) {
-						transformVals.push(this.getRandomWithinRange(-400, 400));	// e.g. [309, -112]
-					}
-					
-					// If transition for 2 dice have extremely short loop time, combine both into one
-					// settimeout loop
-					if (Array.isArray(currDice)) {	// [1, 2]
-						diceObj = {
-							1: {
-								value: diceValues[0],
-								position: {
-									x: step === dieOneLastCycle ? mappedDieTransforms[currDice[0] - 1][0] : transformVals[0],
-									y: step === dieOneLastCycle ? mappedDieTransforms[currDice[0] - 1][1] : transformVals[1],
-									z: step === dieOneLastCycle ? mappedDieTransforms[currDice[0] - 1][2] : transformVals[2]
-								},
-								rollDuration: currDuration[0]
-							},
-							2: {
-								value: diceValues[1],
-								position: {
-									x: step === dieTwoLastCycle ? mappedDieTransforms[currDice[1] - 1][0] : transformVals[3],
-									y: step === dieTwoLastCycle ? mappedDieTransforms[currDice[1] - 1][1] : transformVals[4],
-									z: step === dieTwoLastCycle ? mappedDieTransforms[currDice[1] - 1][2] : transformVals[5]
-								},
-								rollDuration: currDuration[1]
-							}
-						}
-					} else {
-						// Since this is utility code, determine correct last die cycle if the die is 1 or 2
-						const lastDieCycle = currDice === 1 ? dieOneLastCycle : dieTwoLastCycle;
-						diceObj = {
-							[`${ currDice }`]: {
-								value: diceValues[currDice - 1],
-								position: {
-									x: step === lastDieCycle ? mappedDieTransforms[currDice - 1][0] : transformVals[0],
-									y: step === lastDieCycle ? mappedDieTransforms[currDice - 1][1] : transformVals[1],
-									z: step === lastDieCycle ? mappedDieTransforms[currDice - 1][2] : transformVals[2]
-								},
-								rollDuration: currDuration
-							}
-						}
-					}
-
-					this.setState({
-						...this.state,
-						dice: {
-							...this.state.dice,
-							...diceObj
-						}
-					})
-					step++;
-					randomRoll(step)
-				}, step === 0 ? 0 : diceData[0][step] * 1000);	// 1, 2 or 3 (Condition to avoid delay on first run)
-			}
-		}
-		randomRoll(0);
-	}
-
-	rollDice = () => {
-		this.roll();
-	}
 
 	render() {
 		return (
@@ -669,11 +233,9 @@ export default class App extends Component {
 					<section className="roll-series">
 						{/* Double-six multiple rolls */}
 					</section>
-					<section className="roll-button-container">
-						<div id="roll-button" className="roll-button" onClick={(e) => this.rollDice()}>
-							<button className="roll" role="button">Roll</button>
-						</div>
-					</section>
+					<RollBtn
+						setDice={this.setDice}
+					/>
 				</section>
 				<section className="board">
 					<section className="ludo">
---
+++ src/components/RollBtn/RollBtn.js
@@ -0,0 +1,172 @@
+import React, { Component } from 'react';
+import { randomDice, getRandomWithinRange, getDiceCycle, getDiceTimeout, computeDiceData } from '../functions.js';
+
+export default class RollBtn extends Component {
+
+	roll = () => {
+
+		const mapDice = (value) => {
+			let dieTransformX = 0;
+			let dieTransformY = 0;
+			let dieTransformZ = 0;
+
+			const randomTransforms = [360, 0, -360];
+			const diceTransformMap = {	// [x, y coordinates of die value]
+				1 : [0, 0],
+				2 : [270, 0], 
+				3 : [0, 90],
+				4 : [0, 270],
+				5 : [90, 0],
+				6 : [0, 180]    // Also [180, 0]
+			}
+
+			dieTransformX = diceTransformMap[value][0] + randomTransforms[getRandomWithinRange(0, 2, true)] + getRandomWithinRange(0, 10);
+			/*
+				If die value is 2 or 5, run random transform-Y value because die will roll on Y-axis,
+				hence the current die face stays the same.
+				Then flip transform-Z + or -360deg to make a complete spin and return to current die face
+				
+				This piece of code (getRandomWithinRange(0, 10)) gets a random number between 0 and 10 to just tilt
+				the die slightly in random direction to add visual depth. The maximum possible tilt though is not 10 deg
+				but 20 deg because 2 transforms are at play on a die to make the tilt. For e.g, if the die value is 3, 
+				dieTransformX and dieTransformZ if in the same direction and with the maximum possible tilt (10deg), would
+				add to each other to become 20deg
+			*/
+			if (value === 2 || value === 5) {
+				dieTransformY = getRandomWithinRange(-400, 400);
+				dieTransformZ = randomTransforms[getRandomWithinRange(0, 2, true)] + getRandomWithinRange(0, 10);
+			} else if (value === 3 || value === 4) {
+				dieTransformX = randomTransforms[getRandomWithinRange(0, 2, true)] + getRandomWithinRange(0, 10);
+				dieTransformY = diceTransformMap[value][1] + randomTransforms[getRandomWithinRange(0, 2, true)] + getRandomWithinRange(1, 10);
+				dieTransformZ = randomTransforms[getRandomWithinRange(0, 2, true)] + getRandomWithinRange(0, 10);
+			}
+			else {
+				dieTransformY = diceTransformMap[value][1] + randomTransforms[getRandomWithinRange(0, 2, true)] + getRandomWithinRange(0, 10);
+				dieTransformZ = getRandomWithinRange(-400, 400);
+			}
+			return [dieTransformX, dieTransformY, dieTransformZ];
+		}
+
+		const diceValues = [];
+		randomDice(diceValues);
+		const mappedDieTransforms = [mapDice(diceValues[0]), mapDice(diceValues[1])];		
+
+		let cycleSteps = [];	
+		let diceTimeout = [];
+		
+		cycleSteps = getDiceCycle();	// [3, 3]
+
+		diceTimeout[0] = getDiceTimeout(cycleSteps[0]);
+		diceTimeout[1] = getDiceTimeout(cycleSteps[1]);
+
+		// diceTimeout[0] = [1.42, 0.45, 0.35];
+		// diceTimeout[1] = [0.7, 1.09, 1.9];
+
+		const diceData = computeDiceData(diceTimeout);
+		/*
+			Returns something like: 
+			[
+				[0.7, 0.72, 0.44999999999999996, 1.82] 	 // timeout (settransition loop time)
+				[Array(2), 1.09, Array(2), 0.35]	// die transition duration (css)	
+				[Array(2), 2, Array(2), 1]	// dice 
+			]
+		*/
+		console.log(diceData);
+
+		const cycleStep = diceData[0].length;
+
+		let diceArrLastCycle = 0;
+		let dieOneLastCycle = 0;
+		let dieTwoLastCycle = 0;
+		
+		// Catch loop that maps as last loop to original diceTimeout array
+		/* 
+			Determine index of last entries of die 1 and 2.
+			These last indexes of the die 1 and 2 enables the correct application of die value position
+			on final timeout
+		*/
+		if (Array.isArray(diceData[2][diceData[2].length - 1])) {	
+			dieOneLastCycle = diceData[2][diceData[2].length - 1];
+			dieTwoLastCycle = diceData[2][diceData[2].length - 1];
+		} else {
+			diceArrLastCycle = diceData[2].findLastIndex((element) => Array.isArray(element) === true);
+			dieOneLastCycle = diceData[2].findLastIndex((element) => element === 1);
+			dieTwoLastCycle = diceData[2].findLastIndex((element) => element === 2);
+
+			dieOneLastCycle = (dieOneLastCycle > diceArrLastCycle) ? dieOneLastCycle : diceArrLastCycle;
+			dieTwoLastCycle = (dieTwoLastCycle > diceArrLastCycle) ? dieTwoLastCycle : diceArrLastCycle;
+		}
+
+		const randomRoll = (step) => {
+
+			if (step < cycleStep) {
+				
+				setTimeout(() => {
+					let currDuration = diceData[1][step];
+					let currDice = diceData[2][step];
+					let diceObj = {};
+
+					let transformVals = []
+					for (let i = 0; i < (Array.isArray(currDice) ? 6 : 3); i++) {
+						transformVals.push(getRandomWithinRange(-400, 400));	// e.g. [309, -112]
+					}
+					
+					// If transition for 2 dice have extremely short loop time, combine both into one
+					// settimeout loop
+					if (Array.isArray(currDice)) {	// [1, 2]
+						diceObj = {
+							1: {
+								value: diceValues[0],
+								position: {
+									x: step === dieOneLastCycle ? mappedDieTransforms[currDice[0] - 1][0] : transformVals[0],
+									y: step === dieOneLastCycle ? mappedDieTransforms[currDice[0] - 1][1] : transformVals[1],
+									z: step === dieOneLastCycle ? mappedDieTransforms[currDice[0] - 1][2] : transformVals[2]
+								},
+								rollDuration: currDuration[0]
+							},
+							2: {
+								value: diceValues[1],
+								position: {
+									x: step === dieTwoLastCycle ? mappedDieTransforms[currDice[1] - 1][0] : transformVals[3],
+									y: step === dieTwoLastCycle ? mappedDieTransforms[currDice[1] - 1][1] : transformVals[4],
+									z: step === dieTwoLastCycle ? mappedDieTransforms[currDice[1] - 1][2] : transformVals[5]
+								},
+								rollDuration: currDuration[1]
+							}
+						}
+					} else {
+						// Since this is utility code, determine correct last die cycle if the die is 1 or 2
+						const lastDieCycle = currDice === 1 ? dieOneLastCycle : dieTwoLastCycle;
+						diceObj = {
+							[`${ currDice }`]: {
+								value: diceValues[currDice - 1],
+								position: {
+									x: step === lastDieCycle ? mappedDieTransforms[currDice - 1][0] : transformVals[0],
+									y: step === lastDieCycle ? mappedDieTransforms[currDice - 1][1] : transformVals[1],
+									z: step === lastDieCycle ? mappedDieTransforms[currDice - 1][2] : transformVals[2]
+								},
+								rollDuration: currDuration
+							}
+						}
+					}
+
+					this.props.setDice(diceObj)
+					step++;
+					randomRoll(step)
+				}, step === 0 ? 0 : diceData[0][step] * 1000);	// 1, 2 or 3 (Condition to avoid delay on first run)
+			}
+		}
+		randomRoll(0);
+	}
+
+	
+	render() {
+		return (
+			<section className="roll-button-container">
+				<div id="roll-button" className="roll-button" onClick={this.roll}>
+					<button className="roll" role="button">Roll</button>
+				</div>
+			</section>
+		)
+	}
+}
