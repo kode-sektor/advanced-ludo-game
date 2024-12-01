85697815349090d3b5d80d155eddf31c0c8ea3f7
Olagoke Kay <kayodeibiyemi92@gmail.com>
2024-12-01 09:53:10 -0500

    Stage untracked files

---
+++ src/components/DiceWidget/DiceCount/DiceCount.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSolid, faDice } from '@fortawesome/free-solid-svg-icons';
+import { calcMoveDistance } from '../../functions.js';
+
+
+export default class RollBtn extends Component {
+	
+	render() {
+
+		return (
+			<section className='dice-sum'>
+				<h3>
+					Dice Count
+				</h3>
+				<span className="dice-bubble">
+					<FontAwesomeIcon icon={faDice} size='3x' inverse swapOpacity mask={['far', 'circle']}/>
+					<span className='dice-count'>{this.props.moveDistance}</span>
+				</span>
+			</section>
+		)
+	}
+}
